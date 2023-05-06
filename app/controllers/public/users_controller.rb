21519673de244e2018055e4c72434f62e761a977
EC2 Default User <ec2-user@ip-172-31-5-13.ap-northeast-1.compute.internal>
2023-05-06 01:54:41 +0000

    [add] guest login

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,4 +1,7 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_guest_user, only: [:edit]
+
   def show
   end
 
@@ -12,5 +15,14 @@ class Public::UsersController < ApplicationController
   end
 
   def unsubscribe
+  end
+
+  private
+
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "guestuser"
+      redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -32,4 +32,12 @@ class User < ApplicationRecord
     followings.include?(user)
   end
 
+  # ゲストログイン
+  def self.guest
+    find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
+
 end
