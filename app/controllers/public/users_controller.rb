b7a779e2cb6d51c5d857b5e1768fcfc688126d6a
EC2 Default User <ec2-user@ip-172-31-5-13.ap-northeast-1.compute.internal>
2023-05-07 03:45:11 +0000

    [fix] ゲストユーザーのアクセス制限を修正

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,6 +1,6 @@
 class Public::UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :ensure_guest_user, only: [:edit]
+  before_action :ensure_guest_user, only: [:new]
 
   def show
     @user = User.find(params[:id])
