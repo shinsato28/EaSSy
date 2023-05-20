5fce091c7d423a1f6f74c86874b4dce22e8eae75
EC2 Default User <ec2-user@ip-172-31-5-13.ap-northeast-1.compute.internal>
2023-05-20 05:38:23 +0000

    [fix] seedファイルを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,60 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create!(
-  email: 'admin@gmail.com',
+  email: 'admin@test.com',
   password: '111111'
 )
 
 3.times do |n|
     User.create!(
-      email: "test#{n + 1}@gmail.com",
+      email: "test#{n + 1}@test.com",
       password: "111111",
       name: "test#{n + 1}",
       introduction: "初期ユーザー#{n + 1}です"
     )
   end
+
+10.times do |n|
+    Novel.create!(
+      user_id: 1,
+      title: "test1の投稿小説#{n + 1}",
+      body: "投稿された小説の内容#{n + 1}\nです"
+    )
+  end
+
+10.times do |n|
+    Novel.create!(
+      user_id: 2,
+      title: "test2の投稿小説#{n + 1}",
+      body: "投稿された小説の内容#{n + 1}\nです"
+    )
+  end
+
+10.times do |n|
+    Novel.create!(
+      user_id: 3,
+      title: "test3の投稿小説#{n + 1}",
+      body: "投稿された小説の内容#{n + 1}\nです"
+    )
+  end
+
+Tag.create!(
+  name: "tag_name"
+)
+
+novels_count = Novel.all.count
+
+novels_count.times do |n|
+  TagMap.create!(
+    novel_id: (n + 1),
+    tag_id: 1
+  )
+end
+
+novels_count.times do |n|
+  NovelComment.create!(
+    user_id: 1,
+    novel_id: (n + 1),
+    comment: "テストコメント"
+  )
+end
